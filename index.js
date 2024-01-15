4d0d493e09f057e22df139d2321246eb595602a9
albinarcool <150587304+albinarcool@users.noreply.github.com>
2024-01-15 18:45:13 +0100

    added css

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function temperatureConverter(valNum) {
     valNum = parseFloat(valNum);
-    const SuperValue = document.getElementById("outputCelsius").innerHTML = (valNum + 32) * 1.8;
+    const SuperValue = document.getElementById("outputCelsius").innerHTML = (valNum - 32) / 1.8;
     console.log(SuperValue)
   }
+
   
+
+
+  
+
